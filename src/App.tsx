7729f18e63915b3d63149236bc40bc796db13c23
MauroRaya <maurorayabr@gmail.com>
2025-09-21 12:19:47 -0300

    feat: add react router for multiple pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import AuthPage from "./pages/AuthPage";
 import "./styles/auth.css";
 import "./styles/toast.css";
 
 function App() {
-  return <AuthPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/auth" />} />
+
+        <Route path="/auth" element={<AuthPage />} />
+
+        <Route path="*" element={<h1>404 - Página não encontrada</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
